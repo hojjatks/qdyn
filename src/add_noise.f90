41376fa138cfd28e9e7e4540cab07c84e9d93fba
hojjatks <hojjatks@gmail.com>
2024-03-27 12:08:16 -0700

    Understanding where dt is being calculated and writing comments about it.

--- src/add_noise.f90
+++ src/add_noise.f90
@@ -18,7 +18,13 @@ module add_noise
 ! 1- import anything that you think might be needed
 ! 2- I need to think about to which function should I add_noise? solver.f90 or derivis_all.f90
 
-
-
+! ------- To do, remove in future versions
+! Based on what I understood, the time step is adjusted inside the derivis_rk45 function. This function is used inside the solver module
+! when they call rkf45_d:
+!    call rkf45_d( derivs_rk45, neqs, yt, pb%time, pb%tmax, &
+!                  pb%acc, 0d0, pb%rk45%iflag, pb%rk45%work, pb%rk45%iwork) they update the current time step 
+! when they call this line, they update pb%time. Then they say:
+! pb%dt_did=pb%time-pb%t_prev
+! So you have access dt inside the solver module
 
 end module add_noise
