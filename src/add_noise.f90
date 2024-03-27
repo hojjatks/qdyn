d36068bd566cbb6be3a568865eda90300bc784d6
hojjatks <hojjatks@gmail.com>
2024-03-26 18:55:10 -0700

    Adding a few comments on what should I do, and what kinds of variable do I need to import

--- src/add_noise.f90
+++ src/add_noise.f90
@@ -1,4 +1,24 @@
 module add_noise
+! This is the only module that should be changed in order to change the type of the noise.
+! assumptions in the noise:
+
+!-------- To remove in future versions:
+! what do I need from other files.
+! I need the location of the elements (x,y,z)
+! I need the dimensionality of the problem
+! I need to access to the states of the system (V,theta)
+! I need the time step
+! I need to use specific covariance function
+
+!-------- To remove in future versions:
+! 
+!-------- To remove in future versions:
+! What to do?
+! 0- keep cool and do anything step by step!
+! 1- import anything that you think might be needed
+! 2- I need to think about to which function should I add_noise? solver.f90 or derivis_all.f90
+
+
 
 
 end module add_noise
